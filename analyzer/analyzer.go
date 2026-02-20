2f42ea0b15c6e72ae86441514e018a309f61255c
abbottme@student.21-school.ru <shtemisu@DESKTOP-7RE2CL9.localdomain>
2026-02-20 17:52:57 +0900

    Added zap

--- analyzer/analyzer.go
+++ analyzer/analyzer.go
@@ -41,6 +41,7 @@ func run(pass *analysis.Pass) (any, error) {
 	}
 
 	inspect.Nodes(nodeTypes, func(n ast.Node, push bool) bool {
+
 		currentFile := pass.Fset.File(n.Pos())
 
 		if lastFile == nil || currentFile != lastFile {
@@ -55,7 +56,9 @@ func run(pass *analysis.Pass) (any, error) {
 			case *ast.FuncLit:
 				v.Stack.Push("anonymous")
 			case *ast.CallExpr:
-				v.checkLogCall(node)
+				v.checkCallLogSnap(node)
+				v.checkCallZap(node)
+				//	ast.Print(pass.Fset, n)
 			}
 		} else {
 			switch n.(type) {
@@ -63,20 +66,17 @@ func run(pass *analysis.Pass) (any, error) {
 				v.Stack.Pop()
 			}
 		}
-
 		return true
 	})
-
 	return nil, nil
 }
 
-func (v *validator) checkLogCall(call *ast.CallExpr) {
+func (v *validator) checkCallLogSnap(call *ast.CallExpr) {
 	sel, ok := call.Fun.(*ast.SelectorExpr)
 	if !ok {
 		return
 	}
-	if x, ok := sel.X.(*ast.Ident); ok && (x.Name == "log" ||
-		x.Name == "zap" || x.Name == "slog") {
+	if x, ok := sel.X.(*ast.Ident); ok && (x.Name == "log" || x.Name == "slog") {
 		if len(call.Args) == 0 {
 			return
 		}
@@ -85,11 +85,148 @@ func (v *validator) checkLogCall(call *ast.CallExpr) {
 			return
 		}
 		msg := strings.Trim(lit.Value, "\"`")
-		if !rules.IsLower(msg) {
-			v.Pass.Reportf(call.Pos(), "log-message must be start lowercase char")
+		applyRules(msg, call, *v)
+	}
+}
+
+func (v *validator) checkCallZap(call *ast.CallExpr) {
+	sel, ok := call.Fun.(*ast.SelectorExpr)
+	if !ok {
+		return
+	}
+
+	x, ok := sel.X.(*ast.Ident)
+	if !ok || x.Name != "zap" {
+		return
+	}
+	methodName := sel.Sel.Name
+
+	logMethods := map[string]bool{
+		"Debug": true, "Info": true, "Warn": true, "Error": true,
+		"DPanic": true, "Panic": true, "Fatal": true,
+		"Debugf": true, "Infof": true, "Warnf": true, "Errorf": true,
+		"DPanicf": true, "Panicf": true, "Fatalf": true,
+	}
+	if !logMethods[methodName] {
+		return
+	}
+
+	if len(call.Args) == 0 {
+		return
+	}
+	firstArg := call.Args[0]
+	var msg string
+	switch arg := firstArg.(type) {
+	case *ast.CallExpr:
+		msg = checkCallError(arg)
+
+	case *ast.Ident:
+		msg = v.checkValueIdent(arg)
+
+	case *ast.BasicLit:
+		if arg.Kind == token.STRING {
+			msg = strings.Trim(arg.Value, "\"`")
 		}
-		if !rules.OnlyEnglishAndWithoutSpecChar(msg) {
-			v.Pass.Reportf(call.Pos(), "log-message must be in English language")
+	}
+
+	if msg != "" {
+		applyRules(msg, call, *v)
+	}
+}
+
+func checkCallError(expr ast.Expr) string {
+
+	call, ok := expr.(*ast.CallExpr)
+	if !ok {
+		return ""
+	}
+	sel, ok := call.Fun.(*ast.SelectorExpr)
+	if !ok {
+		return ""
+	}
+	pkg, ok := sel.X.(*ast.Ident)
+	if !ok || pkg.Name != "errors" {
+		return ""
+	}
+	if sel.Sel.Name != "New" {
+		return ""
+	}
+
+	if len(call.Args) == 0 {
+		return ""
+	}
+
+	lit, ok := call.Args[0].(*ast.BasicLit)
+	if !ok || lit.Kind != token.STRING {
+		return ""
+	}
+
+	return strings.Trim(lit.Value, "\"`")
+}
+
+func (v *validator) checkValueIdent(ident *ast.Ident) string {
+	obj := v.Pass.TypesInfo.ObjectOf(ident)
+	if obj == nil {
+		return ""
+	}
+
+	for _, file := range v.Pass.Files {
+		var found bool
+		var result string
+		ast.Inspect(file, func(n ast.Node) bool {
+			if found {
+				return false
+			}
+			switch node := n.(type) {
+			case *ast.AssignStmt:
+				for i, lhs := range node.Lhs {
+					lhsIdent, ok := lhs.(*ast.Ident)
+					if !ok {
+						continue
+					}
+					if v.Pass.TypesInfo.ObjectOf(lhsIdent) != obj {
+						continue
+					}
+					if i < len(node.Rhs) {
+						if msg := checkCallError(node.Rhs[i]); msg != "" {
+							result = msg
+							found = true
+							return false
+						}
+					}
+				}
+
+			case *ast.ValueSpec:
+				for i, name := range node.Names {
+					if v.Pass.TypesInfo.ObjectOf(name) != obj {
+						continue
+					}
+
+					if i < len(node.Values) {
+						if msg := checkCallError(node.Values[i]); msg != "" {
+							result = msg
+							found = true
+							return false
+						}
+					}
+				}
+			}
+
+			return true
+		})
+		if found {
+			return result
 		}
+	}
+
+	return ""
+}
+
+func applyRules(msg string, call *ast.CallExpr, v validator) {
+	if !rules.IsLower(msg) {
+		v.Pass.Reportf(call.Pos(), "log-message must be start lowercase char")
+	}
+	if !rules.OnlyEnglishAndWithoutSpecChar(msg) {
+		v.Pass.Reportf(call.Pos(), "log-message must be in English language")
 	}
 }
--- analyzer/testdata/russian.go
+++ analyzer/testdata/russian.go
@@ -2,14 +2,14 @@ package testdata
 
 import (
 	"errors"
-	"log"
-	"log/slog"
 
 	"go.uber.org/zap"
 )
 
 func russianLog() {
-	log.Fatalf("ОШИБКА!")
-	slog.Error("ошибка к подключению")
+	//log.Fatalf("ОШИБКА!")
+	//slog.Error("ошибка к подключению")
 	zap.Error(errors.New("порт занят"))
+	err := errors.New("Ошибка к подключению к БД")
+	zap.Error(err)
 }
